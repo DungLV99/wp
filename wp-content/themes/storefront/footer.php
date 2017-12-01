1e8999da98f783e3e19ce2e59067cda559d8df76
VietDung <vietdung@ringier.com.vn>
2017-12-01 18:02:42 +0700

    a

--- wp-content/themes/storefront/footer.php
+++ wp-content/themes/storefront/footer.php
@@ -9,27 +9,33 @@
 
 ?>
 
-		</div><!-- .col-full -->
-	</div><!-- #content -->
+</div><!-- .col-full -->
+</div><!-- #content -->
 
-	<?php do_action( 'storefront_before_footer' ); ?>
+<?php do_action('storefront_before_footer'); ?>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="col-full">
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="col-full">
 
-			<?php
-			/**
-			 * Functions hooked in to storefront_footer action
-			 *
-			 * @hooked storefront_footer_widgets - 10
-			 * @hooked storefront_credit         - 20
-			 */
-			do_action( 'storefront_footer' ); ?>
 
-		</div><!-- .col-full -->
-	</footer><!-- #colophon -->
+        <?php
+        /**
+         * Functions hooked in to storefront_footer action
+         *
+         * @hooked storefront_footer_widgets - 10
+         * @hooked storefront_credit         - 20
+         */
+        //do_action( 'storefront_footer' );
+        while (have_posts()) :
+            the_post();
+            echo get_the_title();
+        endwhile;
+        ?>
 
-	<?php do_action( 'storefront_after_footer' ); ?>
+    </div><!-- .col-full -->
+</footer><!-- #colophon -->
+
+<?php do_action('storefront_after_footer'); ?>
 
 </div><!-- #page -->
 
